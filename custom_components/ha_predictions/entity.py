2dd74d5ed4acbb543e34dd82c5bda95f623eb5e6
Nils Reiter <nils.reiter@uni-koeln.de>
2026-01-17 06:41:00 +0000

    unique id no longer based on entry

--- custom_components/ha_predictions/entity.py
+++ custom_components/ha_predictions/entity.py
@@ -13,7 +13,6 @@ class HAPredictionEntity(CoordinatorEntity[HAPredictionUpdateCoordinator]):
     def __init__(self, coordinator: HAPredictionUpdateCoordinator) -> None:
         """Initialize."""
         super().__init__(coordinator)
-        self._attr_unique_id = coordinator.config_entry.entry_id
         self._attr_device_info = coordinator.config_entry.runtime_data.device_info
 
     def notify(self, msg: str) -> None:
